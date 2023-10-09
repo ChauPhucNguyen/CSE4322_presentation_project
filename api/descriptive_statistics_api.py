d344f1d3b986767925143e62c6444001469879fc
Vidssk <santiagohernandez120199@gmail.com>
2023-10-09 12:06:30 -0500

    Architecture

--- api/descriptive_statistics_api.py
+++ api/descriptive_statistics_api.py
@@ -3,6 +3,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
+# Descriptive Satistic Functions
+@app.route('/median', methods=['GET','POST'])
+def get_median():
+    return {'median': 10}
+
+@app.route('/descriptivestats', methods=['GET','POST'])
 def get_current_time():
     return {'time': time.time()}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [currentMedian, setMedian] = useState(0);
+
+  useEffect(() => {
+    fetch('/descriptivestats').then(res => res.json())
+    .then(data => { setCurrentTime(data.time); });
+  }, []);
+  useEffect(() => {
+    fetch('/median').then(res => res.json())
+    .then(data => { setMedian(data.median); });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header bg-red-300">
+
+        ... no changes in this part ...
+
+        <p>the median is {currentMedian}</p>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
